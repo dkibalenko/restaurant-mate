74dd4055bad9f513f3dba0b8b791242c830b6b7b
Denys Kibalenko <dennieking87@gmail.com>
2024-08-13 16:10:08 +0200

    Add styles for home page
    - Refactor kitchen index view to display latest 3 dishes
    
    - Refactor restaurant_mate/settings to add staticfiles directory path
    
    - Refactor kitchen/index.html to dishplay latest 3 dishes
    
    - Refactor templates/base.html
    
    - Add static/app.css for styling templates/base.html
    
    - Add kitchen/index.css for styling kitchen/index.html
    
    - Add kitchen/dish.css for styling kitchen/dish.html
    
    - Add kitchen/includes/dish.html

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
-from .models import Cook
+from .models import Dish
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    cooks = Cook.objects.all()
-    return render(request, "kitchen/index.html", {"cooks": cooks})
+    dishes = Dish.objects.all().order_by("-updated_at")[:3]
+    return render(request, "kitchen/index.html", {"dishes": dishes})
