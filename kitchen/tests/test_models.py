47436fb9359841cd7333b79efb235b03c1171289
Denys Kibalenko <dennieking87@gmail.com>
2024-09-09 13:56:29 +0200

    Test: move tests to a separate tests folder
    - Add kitchen/tests directory
    
    - Rename kitchen/tests.py to kitchen/tests/test_models.py
    
    - Add kitchen/tests/__init__.py

--- kitchen/tests/test_models.py
+++ kitchen/tests/test_models.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import DishType
+from ..models import DishType
 
 
 class ModelsTests(TestCase):
