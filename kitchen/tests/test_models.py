14ff23c5fbee7e3ac10176fbe4c91476b1c65681
Denys Kibalenko <dennieking87@gmail.com>
2024-09-09 14:22:58 +0200

    Test/Refactor: add set up method for models tests class and ingredient str test
    - Add setUp method to isolate common set up
    
    - Add ingredient __str__ method test

--- kitchen/tests/test_models.py
+++ kitchen/tests/test_models.py
@@ -2,49 +2,42 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from ..models import DishType
+from kitchen.models import DishType, Ingredient
 
 
 class ModelsTests(TestCase):
-    def test_dish_type_str(self):
-        dish_type = DishType.objects.create(name="Pizza")
-        self.assertEqual(str(dish_type), dish_type.name)
-
-    def test_cook_str(self):
-        cook = get_user_model().objects.create(
+    def setUp(self):
+        self.cook = get_user_model().objects.create_user(
             username="testuser",
             first_name="Test_first_name",
             last_name="Test_last_name",
             password="testpassword"
         )
+        self.dish_type = DishType.objects.create(name="Pizza")
+        self.ingredient = Ingredient.objects.create(name="Water")
+    def test_dish_type_str(self):
+        self.assertEqual(str(self.dish_type), self.dish_type.name)
+
+    def test_cook_str(self):
         self.assertEqual(
-            str(cook), 
-            f"{cook.username}: ({cook.full_name()})"
+            str(self.cook), 
+            f"{self.cook.username}: ({self.cook.full_name()})"
         )
 
     def test_cook_saved_with_slug(self):
-        cook = get_user_model().objects.create(
-            username="testuser", 
-            first_name="Test_first_name", 
-            last_name="Test_last_name", 
-            password="testpassword"
-        )
         self.assertEqual(
-            cook.slug, 
-            f"{cook.first_name.lower()}-{cook.last_name.lower()}"
+            self.cook.slug, 
+            f"{self.cook.first_name.lower()}-{self.cook.last_name.lower()}"
         )
 
-    def test_get_absolute_url(self):
-        cook = get_user_model().objects.create(
-            username="testuser", 
-            first_name="Test_first_name", 
-            last_name="Test_last_name", 
-            password="testpassword"
-        )
+    def test_cook_get_absolute_url(self):
         self.assertEqual(
-            cook.get_absolute_url(), 
+            self.cook.get_absolute_url(), 
             reverse(
                 "kitchen:cook-detail-page", 
-                kwargs={"slug": cook.slug}
+                kwargs={"slug": self.cook.slug}
             )
         )
+
+    def test_ingredient_str(self):
+        self.assertEqual(str(self.ingredient), self.ingredient.name)
