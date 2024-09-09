5db8fd4bbaa39ae7ac86b613918008f19cd30e25
Denys Kibalenko <dennieking87@gmail.com>
2024-09-09 11:22:34 +0200

    Test: configure testing files
    - Add .env with testing configurations
    
    - Update .vscode/settings.json with unittest settings
    
    - Update .gitignore, add .env, .vscode/
    
    - Add dish type __str__ method test

--- kitchen/tests.py
+++ kitchen/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django import setup
+import os
 
-# Create your tests here.
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restaurant_mate.settings")
+# setup()
+
+from .models import DishType
+
+
+class ModelsTests(TestCase):
+    def test_dish_type_str(self):
+        dish_type = DishType.objects.create(name="Pizza")
+        self.assertEqual(str(dish_type), dish_type.name)
