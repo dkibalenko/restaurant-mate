4251a8c26a94379c3604d8fb5ef0bca22db73699
Denys Kibalenko <dennieking87@gmail.com>
2024-09-11 09:27:02 +0200

    Test: add custom logout view tests
    - Add test for get method of the custom logout view. Ensure when get request is made user gets redirected to registration/logged_out.html
    
    - Add test for post method of the custom logout view. Ensure when post request is made user is logged out and redirected to registration/login.html
    
    - Add test to make sure user is logged out.
      PARSE TEST
      1)Logout user
      2)Send get request to protected page with logged out user and store response url
      3)Construct expected url redirect to after trying to access protected page with logged out user
      4)Parse response url and expected redirect url. Assert they equal with .path attribute
      5)Parse response url and expected redirect url query parameters and strip slashes out of next parameter. Assert next parameters are equal

--- kitchen/tests/test_views.py
+++ kitchen/tests/test_views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+from urllib.parse import urlparse, parse_qs
+
 
 class PublicAccessViewTest(TestCase):
     def test_main_page_login_required_and_redirect_to_login_page(self):
@@ -39,4 +41,54 @@ class PublicAccessViewTest(TestCase):
         self.assertRedirects(
             response,
             "/accounts/login/?next=%2Fingredients%2F"
+        )
+
+
+class CustomLogoutViewTest(TestCase):
+    def setUp(self):
+        self.cook = get_user_model().objects.create_user(
+            username="dennie",
+            password="testpassword"
+        )
+        self.client.login(username="dennie", password="testpassword")
+
+    def test_get_logout_view(self):
+        response  = self.client.get(reverse("logout"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/logged_out.html")
+
+    def test_post_logout_view(self):
+        response = self.client.post(reverse("logout"))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("login"))
+        self.assertFalse("_auth_user_id" in self.client.session)
+
+    def test_user_is_logged_out(self):
+        self.client.post(reverse("logout"))
+        response = self.client.get(reverse("kitchen:main-page"))
+        expected_redirect_url = (
+            f"{reverse('login')}?"
+            f"next=/{reverse('kitchen:main-page')}/"
+        )
+
+        parsed_response_url = urlparse(response.url)
+        parsed_expected_redirect_url = urlparse(expected_redirect_url)
+
+        self.assertEqual(
+            parsed_response_url.path, 
+            parsed_expected_redirect_url.path
+        )
+
+        response_next_parameter = (
+            parse_qs(parsed_response_url.query).get("next", [""])[0]
+            .rstrip("/")
+        )
+        expected_redirect_url_next_parameter = (
+            parse_qs(parsed_expected_redirect_url.query).get("next", [""])[0]
+            .rstrip("/")
+        )
+
+        self.assertEqual(
+            response_next_parameter, 
+            expected_redirect_url_next_parameter
         )
