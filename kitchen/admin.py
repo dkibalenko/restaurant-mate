8871b9fe014280ba92bf0f9edd2cc400d92fce5d
Denys Kibalenko <dennieking87@gmail.com>
2024-09-16 18:58:41 +0200

    Chore: reformat kitchen/admin.py to comply wit pep8 formatting

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -17,23 +17,65 @@ class DishTypeAdmin(admin.ModelAdmin):
 @admin.register(Cook)
 class CookAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("bio", "years_of_experience",)
-    # to be used in editing users
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'slug')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-        ('Additional info', {'fields': ('bio', 'years_of_experience', 'profile_picture')}),
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {
+                "fields":
+                ("first_name", "last_name", "email", "slug"),
+            }
+        ),
+        (
+            "Permissions",
+            {
+                "fields":
+                (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions"
+                )
+            }
+        ),
+        (
+            "Important dates",
+            {
+                "fields":
+                ("last_login", "date_joined")
+            }
+        ),
+        (
+            "Additional info",
+            {
+                "fields":
+                ("bio", "years_of_experience", "profile_picture")
+            }
+        ),
     )
-    # to be used when creating a user
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'slug'),
-        }),
-        ('Additional info', {
-            'fields': ('bio', 'years_of_experience', 'profile_picture'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "slug"
+                ),
+            }
+        ),
+        (
+            "Additional info",
+            {
+                "fields": ("bio", "years_of_experience", "profile_picture"),
+            }
+        ),
     )
     search_fields = ("username", "first_name", "last_name",)
     readonly_fields = ("slug",)
