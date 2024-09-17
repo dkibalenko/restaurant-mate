4174e6ffefcb5cf461b1b4ebbdc0811d81e04abf
Denys Kibalenko <dennieking87@gmail.com>
2024-09-17 15:40:08 +0200

    Chore: delete kitchen/mixins.py

--- kitchen/mixins.py
+++
@@ -1,8 +0,0 @@
-class RequiredFieldsMixin:
-    required_fields = []
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.required_fields:
-            if field in self.fields:
-                self.fields[field].required = True
