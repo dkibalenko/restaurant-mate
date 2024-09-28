e58f4d787ab7dcf5979948b90f906fb00d1f3ae1
Denys Kibalenko <dennieking87@gmail.com>
2024-09-28 18:47:00 +0200

    Chore: add url to restaurant_mate/urls.py to server static files from STATIC_URL and STATIC_ROOT

--- restaurant_mate/urls.py
+++ restaurant_mate/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path("", include("kitchen.urls", namespace="kitchen")),
     path("accounts/logout/", LogoutView.as_view(), name="logout"),
     path("accounts/", include("django.contrib.auth.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += debug_toolbar_urls()
